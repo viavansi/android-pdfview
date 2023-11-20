2f4c366536b54bf24e8826d7b52c25e30e564c9a
Vincent TE <vincent.te@infomaniak.com>
2023-11-20 14:27:24 +0100

    Improve getFileName method

--- sample/src/main/java/com/infomaniak/lib/pdfpreview/sample/PDFViewViewModel.kt
+++ sample/src/main/java/com/infomaniak/lib/pdfpreview/sample/PDFViewViewModel.kt
@@ -9,19 +9,21 @@ import androidx.lifecycle.AndroidViewModel
 class PDFViewViewModel(appContext: Application): AndroidViewModel(appContext) {
 
     fun getFileName(contentResolver: ContentResolver, uri: Uri?): String? {
-        var result: String? = null
-        if (uri!!.scheme == "content") {
-            val cursor = contentResolver.query(uri, null, null, null, null)
-            cursor.use { cursor ->
-                if (cursor != null && cursor.moveToFirst()) {
-                    val columnIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-                    result = cursor.getString(columnIndex)
+        return uri?.let { fileUri ->
+            var result: String? = null
+            if (fileUri.scheme == "content") {
+                val cursor = contentResolver.query(fileUri, null, null, null, null)
+                cursor?.use { fileCursor ->
+                    if (fileCursor.moveToFirst()) {
+                        val columnIndex = fileCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                        result = fileCursor.getString(columnIndex)
+                    }
                 }
             }
+            if (result == null) {
+                result = fileUri.lastPathSegment
+            }
+            result
         }
-        if (result == null) {
-            result = uri.lastPathSegment
-        }
-        return result
     }
 }
